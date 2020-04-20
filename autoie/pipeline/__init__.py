3dbb05e9bf10a645499e4deb15e97d7016a7a85a
Yuhan Cheng <chengyh971030@tamu.edu>
2020-04-20 00:25:25 -0400

    'textcnn'

--- autoie/pipeline/__init__.py
+++ autoie/pipeline/__init__.py
@@ -1,3 +1,3 @@
-from autoie.pipeline.blocks import OneHotEmbedding, BiLSTM, Dense, GRU
+from autoie.pipeline.blocks import OneHotEmbedding, BiLSTM, Dense, GRU, TextCNN
 from autoie.pipeline.optimizer import  SparseCategoricalCrossentropyOptimizer, CRFOptimizer
 from autoie.pipeline.node import Input
